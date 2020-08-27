f740b58fed42b0201af27f84dbb715a2c12aafbe
jannicefelicia <jannicesalindeho@gmail.com>
2020-08-27 14:24:43 +0800

    pertemuan 4

---
+++ pertemuan_4/index.js
@@ -0,0 +1,35 @@
+/*
+    1. Function
+*/
+
+// let a = 8;
+// let b = 3;
+// let c = 10;
+// let d = 5;
+
+// let luasA = a * a;
+// let luasB = b * b;
+// let luasC = c * c;
+// let luasD = d * d;
+
+function squareAddition (a, b) {
+    // let luasA = a * a;
+    // let luasB = b * b;
+    // this part of the code is deleted for refactoring (modifying code to make it more concise)
+    //let total = luasA + luasB;
+
+    return total = a * a + b * b;
+}
+
+console.log(squareAddition(8,3));
+console.log(squareAddition(10,5));
+
+function tambah (a, b) {
+    return a + b;
+}
+function kali (a, b) {
+    return a * b;
+}
+
+console.log(tambah(kali(8,3), kali(10,5)));
+//this can be moved above the function unlike other programming languages (this process is called hoisting)
