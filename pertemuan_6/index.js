e02352e800f58364180255d567408dc010c11feb
jannicefelicia <jannicesalindeho@gmail.com>
2020-09-08 14:03:37 +0800

    pertemuan 6

---
+++ pertemuan_6/index.js
@@ -0,0 +1,84 @@
+/*
+    Object
+*/
+
+//const mahasiswa1 = ['John', 'Doe', 31, true];
+//key=index
+
+
+//Mendeklarasikan objek
+//1. Object Literals
+const mhs1 = {
+    namaDepan: 'John',
+    namaBelakang: 'Doe',
+    umur: 31,
+    lulus: true,
+    alamat: {
+        jalan: 'Jl. Arnold Mononutu',
+        kecamatan: 'Airmadidi',
+        kabupaten: 'Minahasa Utara'
+    },
+    IPSemester: [3.05, 3.25, 3.0, 3.4],
+    hitungIP: function() {
+        let total = 0;
+        this.IPSemester.forEach(function(el) {
+            total = total + el;
+        });
+        // return total / 4;
+        this.IPKumulatif = total / 4;
+    }
+};
+
+//2. Kata kunci 'new'
+// const mhs2 = new Object();
+// mhs2.namaDepan = 'John';
+// mhs2.namaBelakang = 'Doe';
+// mhs2.umur = 31;
+// mhs2.lulus = true;
+
+//Mengakses Properti Objek
+//1. Dot Notation
+// console.log(mhs1.umur);
+
+//2. Bracket Notation
+// console.log(mhs1['namaBelakang']);
+
+console.log(mhs1.alamat);
+//accessing an object in an object
+console.log(mhs1.alamat.jalan);
+console.log(mhs1.IPSemester[2]);
+mhs1.hitungIP();
+console.log(mhs1.hitungIP());
+
+//const berlaku pada variabel biasa tetapi tidak berlaku pada objek
+//but if you initialize the previous object (yg pke const), you can't
+
+delete mhs1.namaBelakang;
+console.log(mhs1);
+
+//object is more powerful than array
+//below is contoh array object (JSON)
+//contoh external JSON: postman and placeholder
+const mahasiswa = [
+    {
+        nim: "001",
+        namaDepan: "John",
+        namaBelakang: "Doe"
+    },
+    {
+        nim: "002",
+        namaDepan: "Jane",
+        namaBelakang: "Doe"
+    },
+    {
+        nim: "003",
+        namaDepan: "Chandler",
+        namaBelakang: "Bing"
+    }
+];
+
+mahasiswa.forEach(function(el) {
+    console.log(el);
+});
+
+//when using arrow function (ES6), there will be no used of 'this' keyword
