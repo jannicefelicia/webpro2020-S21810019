d3818bcd13c1466df66d0b38d8ef362ec896c349
jannicefelicia <jannicesalindeho@gmail.com>
2020-10-13 14:28:15 +0800

    pertemuan 16

---
+++ pertemuan_16/hello.js
@@ -0,0 +1,9 @@
+// export const numbers = [1, 2, 3, 4, 5];
+
+import { world } from './component/world.js';
+
+const hello = (user) => {
+    console.log(`hello, ${user}`);
+}
+
+export default hello;
---
+++ pertemuan_16/index.js
@@ -0,0 +1,8 @@
+// import {hello, world, numbers} from './hello.js';
+// import * as helper from './hello.js';
+import hello from './hello.js';
+
+// helper.hello('John Doe');
+hello('John Doe'); //the name depends on the import thing
+// helper.world();
+// console.log(helper.name);
